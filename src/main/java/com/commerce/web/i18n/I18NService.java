5f09ac0f48ee2874a21623ebf9db4aca5bbb5ed4
tomas <t.mikoliunas@gmail.com>
2017-01-14 21:02:54 +0000

    Added Log

--- src/main/java/com/commerce/web/i18n/I18NService.java
+++ src/main/java/com/commerce/web/i18n/I18NService.java
@@ -2,14 +2,19 @@ package com.commerce.web.i18n;
 
 import java.util.Locale;
 
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
 import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.stereotype.Service;
 
+
 @Service
 public class I18NService {
 
+	/** The Application logger */
+	private static final Logger LOG = Logger.getLogger(I18NService.class);
+	
 	@Autowired  // we are injecting 'MessageResource' bean
 	private MessageSource messageSource;
 	
@@ -18,6 +23,8 @@ public class I18NService {
 	 *  @param messageId is the key to the messages resource file
 	 */
 	public String getMessage(String messageId){
+		
+		LOG.info("Returning i18n text for the messageId {}");
 		
 		Locale locale = LocaleContextHolder.getLocale();
 		return getMessage(messageId, locale);
